1635a7c0206266d3cec3336261f33975754470d6
pawel.milecki <pawel.milecki@hycom.pl>
2019-07-21 19:25:22 +0200

    Created configuration.properties.

--- src/test/java/navigation/ApplicationURLs.java
+++ src/test/java/navigation/ApplicationURLs.java
@@ -1,7 +1,9 @@
 package navigation;
 
+import configuration.AppProperties;
+
 public class ApplicationURLs {
 
-    public static final String APPLICATION_URL = "http://przyklady.javastart.pl/jpetstore/";
+    public static final String APPLICATION_URL = AppProperties.getAllUrl();
     public static final String LOGIN_URL = APPLICATION_URL + "actions/Account.action?signonForm=";
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,11 +1,23 @@
 package tests;
 
+import configuration.ConfigurationProperties;
+import configuration.PropertiesLoader;
 import driver.manager.DriverManager;
 import driver.manager.DriverUtils;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
+import java.util.Properties;
+
 public class TestBase {
+
+    @BeforeClass
+    public void beforeClass() {
+        PropertiesLoader propertiesLoader = new PropertiesLoader();
+        Properties propertiesFromFile = propertiesLoader.getPropertiesFromFile("configuration.properties");
+        ConfigurationProperties.setProperties(propertiesFromFile);
+    }
 
     @BeforeMethod
     public void beforeTest() {
