79c7ad4a5b5b9534175e039b5cda802ce1499ad4
pawel.milecki <pawel.milecki@hycom.pl>
2019-07-21 17:41:02 +0200

    Added logger.

--- src/test/java/page/objects/FooterPage.java
+++ src/test/java/page/objects/FooterPage.java
@@ -1,6 +1,8 @@
 package page.objects;
 
 import driver.manager.DriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -8,6 +10,8 @@ import org.openqa.selenium.support.PageFactory;
 import waits.WaitForElement;
 
 public class FooterPage {
+
+    private Logger logger = LogManager.getRootLogger();
 
     @FindBy(css = "#Banner img[src*='dog']")
     private WebElement bannerAfterLoginLogo;
@@ -21,6 +25,7 @@ public class FooterPage {
     public boolean isBannerAfterLoginDisplayed(){
         WaitForElement.waitUntilElementIsVisible(bannerAfterLoginLogo);
         boolean isDisplayed = bannerAfterLoginLogo.isDisplayed();
+        logger.info("Returning status of banner after login: {}", isDisplayed);
         return isDisplayed;
     }
 }
--- src/test/java/page/objects/TopMenuPage.java
+++ src/test/java/page/objects/TopMenuPage.java
@@ -1,6 +1,8 @@
 package page.objects;
 
 import driver.manager.DriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -8,6 +10,8 @@ import org.openqa.selenium.support.PageFactory;
 import waits.WaitForElement;
 
 public class TopMenuPage {
+
+    private Logger logger = LogManager.getRootLogger();
 
     @FindBy(css = "#MenuContent a[href*='signonForm']")
     private WebElement signOnLink;
@@ -21,6 +25,7 @@ public class TopMenuPage {
     public void clickOnSignInLink(){
         WaitForElement.waitUntilElementIsClickable(signOnLink);
         signOnLink.click();
+        logger.info("Clicked on Sign on Link");
     }
 }
 
